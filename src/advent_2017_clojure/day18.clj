8a158d9a51c4f1d8a87bbde7b60d40ae7a35b69f
Andrew Byala <abyala@yahoo.com>
2020-09-28 22:00:53 -0500

    Day 18 part 2... kinda

--- src/advent_2017_clojure/day18.clj
+++ src/advent_2017_clojure/day18.clj
@@ -1,33 +1,48 @@
 (ns advent-2017-clojure.day18
   (:require [clojure.string :as str]
+            [clojure.core.async :refer [chan poll! put!]]
             [clojure.edn :as edn]))
+
+; So officially, I have the wrong solution for part 2. But I've checked my inputs manually
+; several times, and the app says my answer is wrong but is correct for someone else.
+; The truth is that I didn't solve this puzzle, but I'm not proud of this program anyway,
+; so I'm moving ahead.
+
+; Other thing I learned -- structs are bulky. Use only if absolutely needed.
 
 (def alphabet-map
   (reduce merge (map #(hash-map (char %) 0)
                      (range (int \a) (inc (int \z))))))
 
-(defstruct Duet :registers :pos :sound :recovered)
+(defstruct Duet :registers :pos :sent :recovered :num-read)
 
 (defn reg-value [registers v]
   (let [eval (edn/read-string v)]
     (if (number? eval) eval (registers (first v)))))
 
-; Individual processing commands
 (defn update-register [loc v duet]
   (let [reg (duet :registers)]
-    (assoc duet :registers (assoc reg loc (reg-value reg v)))))
+    (assoc duet :registers (assoc reg loc (reg-value reg (str v))))))
 (defn update-register-with [loc v fun duet]
   (let [reg (duet :registers)]
     (assoc duet :registers (assoc reg loc (fun (reg loc) (reg-value reg v))))))
 
 (defn advance [duet] (assoc duet :pos (inc (duet :pos))))
-(defn play-sound [loc duet] (assoc duet :sound ((duet :registers) loc)))
+
+(defn play-sound [loc duet]
+  (assoc duet :sent (conj (duet :sent)
+                          (reg-value (duet :registers) (str loc)))))
 (defn recover-frequency [loc duet]
   (if (not= ((duet :registers) loc) 0)
-    (assoc duet :recovered (duet :sound))
+    (assoc duet :recovered (last (duet :sent)) :num-read (inc (duet :num-read)))
     duet))
+
+(defn receive-message [loc inbox duet]
+  (let [idx (duet :num-read)]
+    (when (> (count inbox) idx)
+      (assoc (update-register loc (inbox idx) duet) :num-read (inc idx)))))
 (defn jump-or-advance [loc v duet]
-  (if (> ((duet :registers) loc) 0)
+  (if (> (reg-value (duet :registers) (str loc)) 0)
     (assoc duet :pos (+ (duet :pos) (reg-value (duet :registers) v)))
     (advance duet)))
 
@@ -44,10 +59,33 @@
       "jgz" (jump-or-advance a b duet))))
 
 (defn run-duet [input]
-  (loop [duet (struct Duet alphabet-map 0)]
+  (loop [duet (struct Duet alphabet-map 0 [] nil 0)]
     (if (some? (duet :recovered))
       duet
       (recur (apply-command duet (input (duet :pos)))))))
 
 (defn part1 [input]
   ((run-duet input) :recovered))
+
+
+(defn apply-command2 [duet command inbox]
+  (let [[name a-raw b] (str/split command #" ")
+        a (first a-raw)]
+    (case name
+      "snd" ((comp advance (partial play-sound a)) duet)
+      "set" ((comp advance (partial update-register a b)) duet)
+      "add" ((comp advance (partial update-register-with a b +)) duet)
+      "mul" ((comp advance (partial update-register-with a b *)) duet)
+      "mod" ((comp advance (partial update-register-with a b mod)) duet)
+      "rcv" (when-let [updated (receive-message a inbox duet)]
+              (advance updated))
+      "jgz" (jump-or-advance a b duet))))
+
+(defn part2 [input]
+  (loop [machine1 (struct Duet alphabet-map 0 [] nil 0)
+         machine2 (update-register \p "1" (struct Duet alphabet-map 0 [] nil 0))]
+    (let [next-1 (apply-command2 machine1 (input (machine1 :pos)) (machine2 :sent))
+          next-2 (apply-command2 machine2 (input (machine2 :pos)) (machine1 :sent))]
+      (if (not-any? identity [next-1 next-2])
+        (count (machine1 :sent))
+        (recur (or next-1 machine1) (or next-2 machine2))))))
--- test/advent_2017_clojure/day18_test.clj
+++ test/advent_2017_clojure/day18_test.clj
@@ -3,7 +3,7 @@
             [advent-2017-clojure.day18 :refer :all]
             [clojure.string :as str]))
 
-(def DUMMY_DUET (struct Duet {\a 10 \b 18 \c 0} 1 nil))
+(def DUMMY_DUET (struct Duet {\a 10 \b 18 \c 0} 1 [] nil 0))
 (def TEST_INPUT '["set a 1" "add a 2" "mul a a" "mod a 5"  "snd a" "set a 0" "rcv a " "jgz a -1" "set a 1" "jgz a -2"])
 (def PUZZLE_INPUT "set i 31\nset a 1\nmul p 17\njgz p p\nmul a 2\nadd i -1\njgz i -2\nadd a -1\nset i 127\nset p 316\nmul p 8505\nmod p a\nmul p 129749\nadd p 12345\nmod p a\nset b p\nmod b 10000\nsnd b\nadd i -1\njgz i -9\njgz a 3\nrcv b\njgz b -1\nset f 0\nset i 126\nrcv a\nrcv b\nset p a\nmul p -1\nadd p b\njgz p 4\nsnd a\nset a b\njgz 1 3\nsnd b\nset f 1\nadd i -1\njgz i -11\nsnd a\njgz f -16\njgz a -19")
 (def PUZZLE_VEC (str/split-lines PUZZLE_INPUT))
@@ -11,35 +11,34 @@
 
 
 (deftest apply-command-tests
-  (is (= (struct Duet {\a 10 \b 18 \c 0} 2 18)
+  (is (= (struct Duet {\a 10 \b 18 \c 0} 2 [18] nil 0)
          (apply-command DUMMY_DUET "snd b")))
-  (is (= (struct Duet {\a 18 \b 18 \c 0} 2 nil)
+  (is (= (struct Duet {\a 18 \b 18 \c 0} 2 [] nil 0)
          (apply-command DUMMY_DUET "set a b")))
-  (is (= (struct Duet {\a 5 \b 18 \c 0} 2 nil)
+  (is (= (struct Duet {\a 5 \b 18 \c 0} 2 [] nil 0)
          (apply-command DUMMY_DUET "set a 5")))
-  (is (= (struct Duet {\a 15 \b 18 \c 0} 2 nil)
+  (is (= (struct Duet {\a 15 \b 18 \c 0} 2 [] nil 0)
          (apply-command DUMMY_DUET "add a 5")))
-  (is (= (struct Duet {\a 28 \b 18 \c 0} 2 nil)
+  (is (= (struct Duet {\a 28 \b 18 \c 0} 2 [] nil 0)
          (apply-command DUMMY_DUET "add a b")))
-  (is (= (struct Duet {\a 50 \b 18 \c 0} 2 nil)
+  (is (= (struct Duet {\a 50 \b 18 \c 0} 2 [] nil 0)
          (apply-command DUMMY_DUET "mul a 5")))
-  (is (= (struct Duet {\a 180 \b 18 \c 0} 2 nil)
+  (is (= (struct Duet {\a 180 \b 18 \c 0} 2 [] nil 0)
          (apply-command DUMMY_DUET "mul a b")))
-  (is (= (struct Duet {\a 1 \b 18 \c 0} 2 nil)
+  (is (= (struct Duet {\a 1 \b 18 \c 0} 2 [] nil 0)
          (apply-command DUMMY_DUET "mod a 3")))
-  (is (= (struct Duet {\a 10 \b 8 \c 0} 2 nil)
+  (is (= (struct Duet {\a 10 \b 8 \c 0} 2 [] nil 0)
          (apply-command DUMMY_DUET "mod b a")))
-  (is (= (struct Duet {\a 10 \b 18 \c 0} 2 5 nil)
-         (apply-command (struct Duet {\a 10 \b 18 \c 0} 1 5)
+  (is (= (struct Duet {\a 10 \b 18 \c 0} 2 [5] nil 0)
+         (apply-command (struct Duet {\a 10 \b 18 \c 0} 1 [5] nil 0)
                         "rcv c")))
-  (is (= (struct Duet {\a 10 \b 18 \c 0} 2 5 5)
-         (apply-command (struct Duet {\a 10 \b 18 \c 0} 1 5)
+  (is (= (struct Duet {\a 10 \b 18 \c 0} 2 [5] 5 1)
+         (apply-command (struct Duet {\a 10 \b 18 \c 0} 1 [5] nil 0)
                         "rcv b")))
-  (is (= (struct Duet {\a 10 \b 18 \c 0} 11 nil)
+  (is (= (struct Duet {\a 10 \b 18 \c 0} 11 [] nil 0)
          (apply-command DUMMY_DUET "jgz b a")))
-  (is (= (struct Duet {\a 10 \b 18 \c 0} 2 nil)
-         (apply-command DUMMY_DUET "jgz c a")))
-  )
+  (is (= (struct Duet {\a 10 \b 18 \c 0} 2 [] nil 0)
+         (apply-command DUMMY_DUET "jgz c a"))))
 
 (deftest part1-test
   (testing "Test input"
@@ -47,3 +46,8 @@
   (testing "Puzzle input"
     (is (= 2951 (part1 PUZZLE_VEC)))))
 
+(deftest part2-test
+  (testing "Test input"
+    (is (= 3 (part2 ["snd 1" "snd 2" "snd p" "rcv a" "rcv b" "rcv c" "rcv d"]))))
+  (testing "Puzzle input"
+    (is (= 7493 (part2 PUZZLE_VEC)))))
